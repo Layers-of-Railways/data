a7cbecf81b12deec5ae59566acb1d7f47a408ee7
IThundxr <contact@ithundxr.dev>
2023-09-18 16:19:45 -0400

    update list
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- gen_json.py
+++ gen_json.py
@@ -8,12 +8,13 @@ with open("dev_capes.txt") as f:
     for line in f:
         names.append(line.strip())
 
-names = list(set(names))
+names = sorted(list(set(names)))
 names = [name for name in names if name != ""]
 
 uuids = {}
 
 for name in names:
+    print("Getting UUID for "+name)
     with urllib.request.urlopen("https://api.ashcon.app/mojang/v2/user/"+name) as f:
         dat = json.load(f)
         uuids[name] = dat["uuid"]
@@ -27,5 +28,7 @@ full_data = {
         } for k, v in uuids.items()
     ]
 }
+
 with open("dev_capes.json", "w") as f:
     json.dump(full_data, f, indent=4)
+    print("Wrote to dev_capes.json")
